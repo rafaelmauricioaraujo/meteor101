f298f7f39c3d2ac4dd2c9e934b1a28b797aa6418
Rafael Araujo <rafael.mauricio.araujo@gmail.com>
2021-10-14 21:49:55 -0300

    retriving data tasks from database

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,23 +1,23 @@
 import React from "react";
+import { useTracker } from "meteor/react-meteor-data";
+import { TaskCollection } from "../api/TaskCollections";
+import { Task } from "./Task";
 import { Hello } from "./Hello.jsx";
 import { Info } from "./Info.jsx";
-import { Task } from "./Task";
 
-const tasks = [
-  { _id: 1, text: "First Task" },
-  { _id: 2, text: "Second Task" },
-  { _id: 3, text: "Third Task" },
-];
+export const App = () => {
+  const tasks = useTracker(() => TaskCollection.find({}).fetch());
 
-export const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello />
-    <ul>
-      {tasks.map((task) => (
-        <Task key={task._id} task={task} />
-      ))}
-    </ul>
-    <Info />
-  </div>
-);
+  return (
+    <div>
+      <h1>Welcome to Meteor!</h1>
+      <Hello />
+      <ul>
+        {tasks.map((task) => (
+          <Task key={task._id} task={task} />
+        ))}
+      </ul>
+      <Info />
+    </div>
+  );
+};
--- server/main.js
+++ server/main.js
@@ -1,31 +1,40 @@
-import { Meteor } from 'meteor/meteor';
-import { LinksCollection } from '/imports/api/links';
+import { Meteor } from "meteor/meteor";
+import { LinksCollection } from "/imports/api/links";
+import { TaskCollection } from "../imports/api/TaskCollections";
 
 function insertLink({ title, url }) {
-  LinksCollection.insert({title, url, createdAt: new Date()});
+  LinksCollection.insert({ title, url, createdAt: new Date() });
+}
+
+function insertTask(taskText) {
+  TaskCollection.insert({ text: taskText });
 }
 
 Meteor.startup(() => {
+  if (TaskCollection.find().count() === 0) {
+    ["First", "Second", "Third"].forEach(insertTask);
+  }
+
   // If the Links collection is empty, add some data.
   if (LinksCollection.find().count() === 0) {
     insertLink({
-      title: 'Do the Tutorial',
-      url: 'https://www.meteor.com/tutorials/react/creating-an-app'
+      title: "Do the Tutorial",
+      url: "https://www.meteor.com/tutorials/react/creating-an-app",
     });
 
     insertLink({
-      title: 'Follow the Guide',
-      url: 'http://guide.meteor.com'
+      title: "Follow the Guide",
+      url: "http://guide.meteor.com",
     });
 
     insertLink({
-      title: 'Read the Docs',
-      url: 'https://docs.meteor.com'
+      title: "Read the Docs",
+      url: "https://docs.meteor.com",
     });
 
     insertLink({
-      title: 'Discussions',
-      url: 'https://forums.meteor.com'
+      title: "Discussions",
+      url: "https://forums.meteor.com",
     });
   }
 });
