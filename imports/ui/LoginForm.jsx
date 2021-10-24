c9066b1395d5f7363933c517ae50e99966fcc3d9
Rafael Araujo <rafael.mauricio.araujo@gmail.com>
2021-10-23 22:03:47 -0300

    creating login form

---
+++ imports/ui/LoginForm.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { Meteor } from "meteor/meteor";
+
+const LoginForm = () => {
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
+
+    const handlerInputUsername = (event) => {
+        setUsername(trim(event.target.value));
+    };
+
+    const handlerInputPassword = (event) => {
+        setPassword(trim(event.target.value));
+    };
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        Meteor.loginWithPassword(username, password);
+    };
+
+    return (
+        <form className="login-form" onSubmit={onSubmit}>
+            <label htmlFor="username">Username</label>
+            <input
+                type="text"
+                placeholder="Username"
+                name="username"
+                required
+                onChange={handlerInputUsername}
+            />
+
+            <label htmlFor="password">Password</label>
+            <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                required
+                onChange={handlerInputPassword}
+            />
+            <button type="submit">Log in </button>
+        </form>
+    );
+};
+
+export default LoginForm;
