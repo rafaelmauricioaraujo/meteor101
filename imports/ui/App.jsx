f92ff166e4855beabe3310ed6431d7a44a50742f
Rafael Araujo <rafael.mauricio.araujo@gmail.com>
2021-10-23 22:11:47 -0300

    adding login form to app

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,76 +1,87 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
+import { Meteor } from "meteor/meteor";
 import { useTracker } from "meteor/react-meteor-data";
 import { TaskCollection } from "../api/TaskCollections";
 import { Task } from "./Task";
 import { Info } from "./Info.jsx";
 import TaskForm from "./TaskForm";
+import LoginForm from "./LoginForm";
 import "../../client/main.css";
 
 export const App = () => {
-  const [hideCompleted, setHideCompleted] = useState(false);
+    const user = useTracker(() => Meteor.user());
+    console.log("user: ", user);
 
-  const hideCompletedFilter = { isChecked: { $ne: true } };
+    const [hideCompleted, setHideCompleted] = useState(false);
 
-  const tasks = useTracker(() =>
-    TaskCollection.find(hideCompleted ? hideCompletedFilter : {}, {
-      sort: { createdAt: -1 },
-    }).fetch()
-  );
+    const hideCompletedFilter = { isChecked: { $ne: true } };
 
-  const pendingTasksCount = useTracker(() => {
-    TaskCollection.find(hideCompletedFilter).count();
-  });
+    const tasks = useTracker(() =>
+        TaskCollection.find(hideCompleted ? hideCompletedFilter : {}, {
+            sort: { createdAt: -1 },
+        }).fetch()
+    );
 
-  const pendingTaskTitle = `${
-    pendingTasksCount ? ` (${pendingTasksCount})` : ""
-  }`;
-
-  function handlerCheck({ _id, isChecked }) {
-    TaskCollection.update(_id, {
-      $set: {
-        isChecked: isChecked,
-      },
+    const pendingTasksCount = useTracker(() => {
+        TaskCollection.find(hideCompletedFilter).count();
     });
-  }
 
-  function deleteTask({ _id }) {
-    TaskCollection.remove(_id);
-  }
+    const pendingTaskTitle = `${
+        pendingTasksCount ? ` (${pendingTasksCount})` : ""
+    }`;
 
-  function handleButton() {
-    setHideCompleted(!hideCompleted);
-  }
+    function handlerCheck({ _id, isChecked }) {
+        TaskCollection.update(_id, {
+            $set: {
+                isChecked: isChecked,
+            },
+        });
+    }
 
-  return (
-    <div className="app">
-      <header>
-        <div className="app-bar">
-          <div className="app-header">
-            <h1>📝️ To Do List {pendingTaskTitle}</h1>
-          </div>
+    function deleteTask({ _id }) {
+        TaskCollection.remove(_id);
+    }
+
+    function handleButton() {
+        setHideCompleted(!hideCompleted);
+    }
+
+    return (
+        <div className="main">
+            {user ? (
+                <Fragment>
+                    <header>
+                        <div className="app-bar">
+                            <div className="app-header">
+                                <h1>📝️ To Do List {pendingTaskTitle}</h1>
+                            </div>
+                        </div>
+                    </header>
+
+                    <div className="main">
+                        <TaskForm />
+
+                        <ul className="tasks">
+                            {tasks.map((task) => (
+                                <Task
+                                    key={task._id}
+                                    task={task}
+                                    onCheck={handlerCheck}
+                                    deleteTask={deleteTask}
+                                />
+                            ))}
+                        </ul>
+                        <div className="filter">
+                            <button onClick={handleButton}>
+                                {hideCompleted ? "Show all" : "Hide completed"}
+                            </button>
+                        </div>
+                        <Info />
+                    </div>
+                </Fragment>
+            ) : (
+                <LoginForm />
+            )}
         </div>
-      </header>
-
-      <div className="main">
-        <TaskForm />
-
-        <ul className="tasks">
-          {tasks.map((task) => (
-            <Task
-              key={task._id}
-              task={task}
-              onCheck={handlerCheck}
-              deleteTask={deleteTask}
-            />
-          ))}
-        </ul>
-        <div className="filter">
-          <button onClick={handleButton}>
-            {hideCompleted ? "Show all" : "Hide completed"}
-          </button>
-        </div>
-        <Info />
-      </div>
-    </div>
-  );
+    );
 };
